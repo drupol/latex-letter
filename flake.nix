1adda2f16a88ef00c17da1a229764b01e6b92f56
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-12-24 11:34:17 +0100

    nix: fix flakes and add default formatter

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "LaTeX Letter";
+  description = "Pandoc letters";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
@@ -14,40 +14,46 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, pandoc-letter, pandoc-scrlttr2, ... }@inputs:
-    let
-      overlay = nixpkgs: final: prev: {
-        pandoc-letter-template = final.stdenvNoCC.mkDerivation {
-          name = "pandoc-letter-template";
-          src = pandoc-letter;
-          dontBuild = true;
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    pandoc-letter,
+    pandoc-scrlttr2,
+    ...
+  } @ inputs: let
+    overlay = nixpkgs: final: prev: {
+      pandoc-letter-template = final.stdenvNoCC.mkDerivation {
+        name = "pandoc-letter-template";
+        src = pandoc-letter;
+        dontBuild = true;
 
-          installPhase = ''
-            runHook preInstall
+        installPhase = ''
+          runHook preInstall
 
-            mkdir -p $out/share/pandoc/templates/
-            cp $src/template-letter.tex $out/share/pandoc/templates/letter.tex
+          mkdir -p $out/share/pandoc/templates/
+          cp $src/template-letter.tex $out/share/pandoc/templates/letter.tex
 
-            runHook postInstall
-          '';
-        };
-
-        pandoc-letter-scrlttr2-template = final.stdenvNoCC.mkDerivation {
-          name = "pandoc-letter-scrlttr2-template";
-          src = pandoc-scrlttr2;
-          dontBuild = true;
-
-          installPhase = ''
-            runHook preInstall
-
-            mkdir -p $out/share/pandoc/templates/
-            cp $src/scrlttr2.latex $out/share/pandoc/templates/scrlttr2.tex
-
-            runHook postInstall
-          '';
-        };
+          runHook postInstall
+        '';
       };
-    in
+
+      pandoc-letter-scrlttr2-template = final.stdenvNoCC.mkDerivation {
+        name = "pandoc-letter-scrlttr2-template";
+        src = pandoc-scrlttr2;
+        dontBuild = true;
+
+        installPhase = ''
+          runHook preInstall
+
+          mkdir -p $out/share/pandoc/templates/
+          cp $src/scrlttr2.latex $out/share/pandoc/templates/scrlttr2.tex
+
+          runHook postInstall
+        '';
+      };
+    };
+  in
     {
       overlays.default = overlay nixpkgs;
 
@@ -56,120 +62,131 @@
         path = ./template;
         description = "A template for creating beautiful letters with Pandoc.";
       };
-    } //
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          overlays = [
-            self.overlays.default
-          ];
-          inherit system;
+    }
+    // flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        overlays = [
+          self.overlays.default
+        ];
+        inherit system;
+      };
+
+      tex = pkgs.texlive.combine {
+        inherit (pkgs.texlive) scheme-full latex-bin latexmk;
+      };
+
+      pandoc-templates = pkgs.symlinkJoin {
+        name = "pandoc-templates";
+
+        paths = [
+          pkgs.pandoc-letter-scrlttr2-template
+          pkgs.pandoc-letter-template
+        ];
+      };
+
+      pandoc = pkgs.writeShellApplication {
+        name = "pandoc";
+        text = ''
+          ${pkgs.pandoc}/bin/pandoc --data-dir=${pandoc-templates}/share/pandoc/ "$@"
+        '';
+        runtimeInputs = [tex];
+      };
+
+      letter = pkgs.stdenvNoCC.mkDerivation {
+        name = "latex-letter";
+
+        src = pkgs.lib.cleanSource ./.;
+
+        buildInputs = [
+          tex
+          pandoc
+          pkgs.gnumake
+        ];
+
+        buildPhase = ''
+          make -C template build-letter
+        '';
+
+        installPhase = ''
+          runHook preInstall
+
+          install -m644 -D template/*.pdf --target $out/
+
+          runHook postInstall
+        '';
+      };
+
+      letter-scrlttr2 = pkgs.stdenvNoCC.mkDerivation {
+        name = "latex-letter-scrlttr2";
+
+        src = pkgs.lib.cleanSource ./.;
+
+        buildInputs = [
+          tex
+          pandoc
+          pkgs.gnumake
+        ];
+
+        buildPhase = ''
+          make -C template build-letter-scrlttr2
+        '';
+
+        installPhase = ''
+          runHook preInstall
+
+          install -m644 -D template/*.pdf --target $out/
+
+          runHook postInstall
+        '';
+      };
+    in {
+      formatter = pkgs.alejandra;
+
+      apps = {
+        pandoc = flake-utils.lib.mkApp {
+          drv = pandoc;
         };
-
-        tex = pkgs.texlive.combine {
-          inherit (pkgs.texlive) scheme-full latex-bin latexmk;
-        };
-
-        pandoc-templates = pkgs.symlinkJoin {
-          name = "pandoc-templates";
-
-          paths = [
-            pkgs.pandoc-letter-scrlttr2-template
-            pkgs.pandoc-letter-template
-          ];
-        };
-
-        pandoc = pkgs.writeShellApplication {
-          name = "pandoc";
-          text = ''
-            ${pkgs.pandoc}/bin/pandoc --data-dir=${pandoc-templates}/share/pandoc/ "$@"
-          '';
-          runtimeInputs = [ tex ];
-        };
-
-        letter = pkgs.stdenvNoCC.mkDerivation {
-          name = "latex-letter";
-
-          src = pkgs.lib.cleanSource ./.;
-
-          buildInputs = [
-            tex
-            pandoc
-            pkgs.gnumake
-          ];
-
-          buildPhase = ''
-            make -C template build-letter
-          '';
-
-          installPhase = ''
-            runHook preInstall
-
-            install -m644 -D template/*.pdf --target $out/
-
-            runHook postInstall
-          '';
-        };
-
-        letter-scrlttr2 = pkgs.stdenvNoCC.mkDerivation {
-          name = "latex-letter-scrlttr2";
-
-          src = pkgs.lib.cleanSource ./.;
-
-          buildInputs = [
-            tex
-            pandoc
-            pkgs.gnumake
-          ];
-
-          buildPhase = ''
-            make -C template build-letter-scrlttr2
-          '';
-
-          installPhase = ''
-            runHook preInstall
-
-            install -m644 -D template/*.pdf --target $out/
-
-            runHook postInstall
-          '';
-        };
-      in
-      {
-        packages = {
-          pandoc = pandoc;
-          letter = pkgs.writeShellApplication {
+        letter = flake-utils.lib.mkApp {
+          drv = pkgs.writeShellApplication {
             name = "pandoc-letter-app";
             text = ''
               ${pandoc}/bin/pandoc -s --template=letter.tex "$@"
             '';
-            runtimeInputs = [ tex ];
+            runtimeInputs = [tex];
           };
-          letter-scrlttr2 = pkgs.writeShellApplication {
+        };
+        letter-scrlttr2 = flake-utils.lib.mkApp {
+          drv = pkgs.writeShellApplication {
             name = "pandoc-letter-scrlttr2-app";
             text = ''
               ${pandoc}/bin/pandoc -s --template=scrlttr2.tex "$@"
             '';
-            runtimeInputs = [ tex ];
+            runtimeInputs = [tex];
           };
         };
+      };
 
-        # Nix develop
-        devShells.default = pkgs.mkShellNoCC {
-          name = "latex-letter-devShell";
-          buildInputs = [
-            tex
-            pandoc
-            pkgs.nodePackages.prettier
-            pkgs.nixpkgs-fmt
-            pkgs.nixfmt
-          ];
-        };
+      packages = {
+        letter = letter;
+        letter-scrlttr2 = letter-scrlttr2;
+      };
 
-        checks = {
-          letter = letter;
-          letter-scrlttr2 = letter-scrlttr2;
-          pandoc = pandoc;
-        };
-      });
+      # Nix develop
+      devShells.default = pkgs.mkShellNoCC {
+        name = "latex-letter-devShell";
+        buildInputs = [
+          tex
+          pandoc
+          pkgs.nodePackages.prettier
+          pkgs.nixpkgs-fmt
+          pkgs.nixfmt
+        ];
+      };
+
+      checks = {
+        letter = letter;
+        letter-scrlttr2 = letter-scrlttr2;
+        pandoc = pandoc;
+      };
+    });
 }
--- template/flake.nix
+++ template/flake.nix
@@ -8,92 +8,99 @@
     latex-letter.url = "github:drupol/latex-letter";
   };
 
-  outputs = { self, nixpkgs, flake-utils, nix-filter, latex-letter, ... }@inputs:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        version = self.shortRev or self.lastModifiedDate;
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    nix-filter,
+    latex-letter,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system: let
+      version = self.shortRev or self.lastModifiedDate;
 
-        pkgs = import nixpkgs {
-          inherit system;
+      pkgs = import nixpkgs {
+        inherit system;
 
-          overlays = [
-            latex-letter.overlays.default
-          ];
-        };
+        overlays = [
+          latex-letter.overlays.default
+        ];
+      };
 
-        tex = pkgs.texlive.combine {
-          inherit (pkgs.texlive) scheme-full latex-bin latexmk;
-        };
+      tex = pkgs.texlive.combine {
+        inherit (pkgs.texlive) scheme-full latex-bin latexmk;
+      };
 
-        pandoc = pkgs.writeShellApplication {
-          name = "pandoc";
-          text = ''
-            ${pkgs.pandoc}/bin/pandoc --data-dir=${pkgs.pandoc-letter-template}/share/pandoc/ "$@"
-          '';
-          runtimeInputs = [ tex ];
-        };
+      pandoc = pkgs.writeShellApplication {
+        name = "pandoc";
+        text = ''
+          ${pkgs.pandoc}/bin/pandoc --data-dir=${pkgs.pandoc-letter-template}/share/pandoc/ "$@"
+        '';
+        runtimeInputs = [tex];
+      };
 
-        letter = pkgs.stdenvNoCC.mkDerivation {
-          name = "pandoc-letter";
+      letter = pkgs.stdenvNoCC.mkDerivation {
+        name = "pandoc-letter";
 
-          src = pkgs.lib.cleanSource ./.;
+        src = pkgs.lib.cleanSource ./.;
 
-          nativeBuildInputs = [
-            pandoc
-            tex
-          ];
+        nativeBuildInputs = [
+          pandoc
+          tex
+        ];
 
-          build = ''
-            make build-letter
-          '';
+        build = ''
+          make build-letter
+        '';
 
-          installPhase = ''
-            runHook preInstall
+        installPhase = ''
+          runHook preInstall
 
-            install -m644 -D letter.pdf --target $out/
+          install -m644 -D letter.pdf --target $out/
 
-            runHook postInstall
-          '';
-        };
+          runHook postInstall
+        '';
+      };
 
-        letter-scrlttr2 = pkgs.stdenvNoCC.mkDerivation {
-          name = "pandoc-letter-scrlttr2";
+      letter-scrlttr2 = pkgs.stdenvNoCC.mkDerivation {
+        name = "pandoc-letter-scrlttr2";
 
-          src = pkgs.lib.cleanSource ./.;
+        src = pkgs.lib.cleanSource ./.;
 
-          nativeBuildInputs = [
-            pandoc
-            tex
-          ];
+        nativeBuildInputs = [
+          pandoc
+          tex
+        ];
 
-          build = ''
-            make build-letter-scrlttr2
-          '';
+        build = ''
+          make build-letter-scrlttr2
+        '';
 
-          installPhase = ''
-            runHook preInstall
+        installPhase = ''
+          runHook preInstall
 
-            install -m644 -D letter.pdf --target $out/
+          install -m644 -D letter.pdf --target $out/
 
-            runHook postInstall
-          '';
-        };
-      in
-      {
-        # Nix shell / nix build
-        packages.letter = letter;
-        packages.letter-scrlttr2 = letter-scrlttr2;
+          runHook postInstall
+        '';
+      };
+    in {
+      formatter = pkgs.alejandra;
 
-        # Nix develop
-        devShells.default = pkgs.mkShellNoCC {
-          name = "letter-devshell";
-          buildInputs = [
-            tex
-            pandoc
-            pkgs.gnumake
-            pkgs.nixpkgs-fmt
-            pkgs.nixfmt
-          ];
-        };
-      });
+      # Nix shell / nix build
+      packages.letter = letter;
+      packages.letter-scrlttr2 = letter-scrlttr2;
+
+      # Nix develop
+      devShells.default = pkgs.mkShellNoCC {
+        name = "letter-devshell";
+        buildInputs = [
+          tex
+          pandoc
+          pkgs.gnumake
+          pkgs.nixpkgs-fmt
+          pkgs.nixfmt
+        ];
+      };
+    });
 }
